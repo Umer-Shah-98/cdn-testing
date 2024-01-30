44f7edf0c0dd568c3caf074752a43a0288ce4d01
Umer Shah <umer@shah.com>
2024-01-30 16:20:21 +0500

    testing of chat-app cdn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 console.log("This is my CDN testing.");
 document.write("Hey this is my test CDN file here.");
 document.write("Hey this is my test CDN file for chatbot app.");
+const chatButtonEl = document.getElementById("chat-button");
+const iframeDivEl = document.getElementById("iframe-chat-app");
+
+let isIframeVisible = true;
+
+chatButtonEl.addEventListener("click", () => {
+  console.log("Chat button clicked");
+  if (isIframeVisible) {
+    iframeDivEl.style.display = "none";
+  } else {
+    iframeDivEl.style.display = "block";
+  }
+  isIframeVisible = !isIframeVisible;
+});
