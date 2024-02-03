8ab3e306d0944bf94dd88acf290fb77d9ebda4b5
Umer Shah <umer@shah.com>
2024-02-03 11:49:23 +0500

    tested implementation of adding custom libraries through CDN

---
+++ libraries/myLibraryName/index.js
@@ -0,0 +1,11 @@
+ const textAccordingToType = (type) => {
+    if (type === "onTraining") {
+      return "No chatbot is currently undergoing training.";
+    } else if (type === "active") {
+      return "No chatbot is currently  active.";
+    } else if (type === "inactive") {
+      return "No chatbot is currently  inactive.";
+    } else {
+      return "No chatbot is currently  saved.";
+    }
+  };
